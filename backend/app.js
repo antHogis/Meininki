7fe04f07d8d421a655ba7531b6efb700005483fa
Anton Höglund <antonjhoglund@hotmail.com>
2019-10-07 14:15:42 +0300

    Enable cors request from localhost:8080

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv/config');
 
 const indexRouter = require('./routes/index');
@@ -11,6 +12,7 @@ const eventsRouter = require('./routes/events');
 
 const app = express();
 
+app.use(cors({origin:'http://localhost:8080'}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
