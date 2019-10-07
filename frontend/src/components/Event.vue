db6f14bfeb34bfbff50ec96dd5ab73dc0a61f156
Anton Höglund <antonjhoglund@hotmail.com>
2019-10-07 12:47:47 +0300

    Create simple Event component content format

--- frontend/src/components/Event.vue
+++ frontend/src/components/Event.vue
@@ -1,13 +1,23 @@
 <template>
-    
+    <div class="event">
+        <h1> {{ event.title }} </h1>
+        <p> {{ event.description }} </p>
+    </div>
 </template>
 
 <script>
 export default {
-    
+    name: 'Event',
+    props: {
+        event: Object
+    },
 }
 </script>
 
 <style scoped>
-
+.event {
+    background-color: #6d6b6d;
+    margin: 1% 1% 1%;
+    padding: 0% 1% 0%;
+}
 </style>
