b917f1ead37ac52dfb7943559f40b364b936f3ef
Anton Höglund <antonjhoglund@hotmail.com>
2019-10-07 18:38:29 +0300

    Add route to Event vue

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -18,6 +18,12 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/event/:id',
+      name: 'eventById',
+      component: () => import('./views/Event.vue'),
+      props: true
     }
   ]
 })
