d6b0b70d96a23001a50102bacfacc3dd85fba58b
Anton Höglund <antonjhoglund@hotmail.com>
2019-10-07 16:02:56 +0300

    Add styling to Event.vue

--- frontend/src/components/Event.vue
+++ frontend/src/components/Event.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="event">
-        <h1> {{ event.title }} </h1>
-        <p> {{ event.description }} </p>
+        <div class="imagePlaceholder"> </div>
+        <div class="info">
+            <h1> {{ event.title }} </h1>
+            <p> {{ event.description }} </p>
+        </div>
     </div>
 </template>
 
@@ -16,8 +19,31 @@ export default {
 
 <style scoped>
 .event {
-    background-color: #6d6b6d;
+    background-color: #413941;
     margin: 1% 1% 1%;
-    padding: 0% 1% 0%;
+    padding: 1% 1% 1%;
+    display: flex;
+
+    border-radius: 5px;
+    border-style: solid;
+    border-width: 1px;
+    border-color: #5c515c;
+}
+
+.imagePlaceholder {
+    background-color: #684c68;
+    width: 200px;
+    height: 150px;
+}
+
+.info {
+    display: inline-block;
+    margin-left: 1vw;
+}
+
+h1, p {
+    color: white;
+    font-family: 'Open Sans', sans-serif;
+    margin: 0%;
 }
 </style>
