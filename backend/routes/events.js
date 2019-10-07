c796216964a3ad4c94151d5d5b711ec36dbc1646
Anton Höglund <antonjhoglund@hotmail.com>
2019-10-07 12:26:44 +0300

    Add get by id event route

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -7,6 +7,12 @@ router.get('/', (req, res) => {
         .then(data => res.json(data));
 })
 
+router.get('/:id', (req, res) => {
+    let id = req.params.id;
+    Event.findById(id)
+        .then(data => res.json(data));
+});
+
 router.post('/', (req, res) => {
     const rb = req.body;
     const event = new Event({
