ef4450f7d68411c56427cb404a9dc4c08c0cd84d
Anton Höglund <antonjhoglund@hotmail.com>
2019-10-07 12:28:12 +0300

    Reformat indentation in events.js

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -3,24 +3,24 @@ const router = express.Router();
 const Event = require('../models/Event');
 
 router.get('/', (req, res) => {
-    Event.find()
-        .then(data => res.json(data));
+  Event.find()
+    .then(data => res.json(data));
 })
 
 router.get('/:id', (req, res) => {
-    let id = req.params.id;
-    Event.findById(id)
-        .then(data => res.json(data));
+  let id = req.params.id;
+  Event.findById(id)
+    .then(data => res.json(data));  
 });
 
 router.post('/', (req, res) => {
-    const rb = req.body;
-    const event = new Event({
-        title: rb.title,
-        description: rb.description
-    });
+  const rb = req.body;
+  const event = new Event({
+    title: rb.title,
+    description: rb.description
+  });
 
-    event.save()
+  event.save()
     .then(data => res.json(data))
     .catch(err => res.json({ message: err }));
 });
