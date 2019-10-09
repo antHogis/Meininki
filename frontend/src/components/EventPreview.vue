4a7bbd29fac2c63d64001aec46d40af64658a03c
Anton Höglund <antonjhoglund@hotmail.com>
2019-10-09 23:44:55 +0300

    Apply 2 spaces indent to EventPreview

--- frontend/src/components/EventPreview.vue
+++ frontend/src/components/EventPreview.vue
@@ -1,89 +1,86 @@
 <template>
-    <div class="event" v-on:click="routeToEvent">
-        <img v-if="event.imageUrl" v-bind:src="event.imageUrl" class="image" />
-        <div v-else class="image"> </div>
-        
-        <div class="info">
-            <h1> {{ event.title }} {{ simpleDate }}</h1>
-            <p> {{ event.description }} </p>
-            
-        </div>
-        
+  <div class="event" v-on:click="routeToEvent">
+    <img v-if="event.imageUrl" v-bind:src="event.imageUrl" class="image" />
+    <div v-else class="image"> </div>
+    <div class="info">
+      <h1> {{ event.title }} {{ simpleDate }}</h1>
+      <p> {{ event.description }} </p>
     </div>
+  </div>
 </template>
 
 <script>
 import router from '../router';
 export default {
-    name: 'EventPreview',
-    props: {
-        event: Object
-    },
-    data() {
-        let dateObject = new Date(this.event.timeStart);
-        return {
-            simpleDate: dateObject.getDay() + '.' + dateObject.getMonth()
-        }
-    },
-    methods: {
-        routeToEvent(){
-            router.push({name:'eventById', params: { id: this.$props.event._id }})
-        }
+  name: 'EventPreview',
+  props: {
+    event: Object
+  },
+  data() {
+    let dateObject = new Date(this.event.timeStart);
+    return {
+      simpleDate: dateObject.getDay() + '.' + dateObject.getMonth()
     }
+  },
+  methods: {
+    routeToEvent(){
+      router.push({name:'eventById', params: { id: this.$props.event._id }})
+    }
+  }
 }
 </script>
 
 <style scoped>
 .event {
-    background-color: #413941;
-    margin: 0.5% 1% 0.25%;
-    padding: 1% 1% 1%;
-    display: flex;
+  background-color: #413941;
+  margin: 0.5% 1% 0.25%;
+  padding: 1% 1% 1%;
+  display: flex;
 
-    border-radius: 5px;
-    border-style: solid;
-    border-width: 1px;
-    border-color: #5c515c;
+  border-radius: 5px;
+  border-style: solid;
+  border-width: 1px;
+  border-color: #5c515c;
 }
 
 .event:hover {
-    border-color: #807080;
+  border-color: #807080;
 }
 
 .image {
-    background-color: #684c68;
-    object-fit: cover !important;
+  background-color: #684c68;
+  object-fit: cover !important;
 }
 
 .info {
-    display: inline-block;
-    margin-left: 1vw;
+  display: inline-block;
+  margin-left: 1vw;
 }
 
 h1, p {
-    color: white;
-    font-family: 'Open Sans', sans-serif;
-    margin: 0%;
+  color: white;
+  font-family: 'Open Sans', sans-serif;
+  margin: 0%;
 }
 
 @media only screen and (min-width: 500px) {
-    .image {
-        width: 200px;
-        height: 150px;
-    }
+  .image {
+      width: 200px;
+      height: 150px;
+  }
 }
 
 @media only screen and (max-width: 500px) {
-    .event {
-        overflow: auto;
-        flex-direction: column;
-    }
+  .event {
+      overflow: auto;
+      flex-direction: column;
+  }
 
-    .image {
-        flex: 1;
-        flex-basis: 200px;
-        height: 300px;
-        width: auto;
-    }
+  .image {
+      flex: 1;
+      flex-basis: 200px;
+      height: 300px;
+      width: auto;
+  }
 } 
 </style>
