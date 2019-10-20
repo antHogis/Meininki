8949775cd040d723e54e0ac528eac806cfd15ddb
Anton Höglund <antonjhoglund@hotmail.com>
2019-10-20 16:12:38 +0300

    Fetch event data in Event.vue

--- frontend/src/views/Event.vue
+++ frontend/src/views/Event.vue
@@ -5,8 +5,32 @@
 <script>
 export default {
   name: 'Event',
-  props: ['id'],
-
+  props: {
+    id: String
+  },
+  data() {
+    return {
+      loading: false,
+      error: null,
+      event: null,
+    }
+  },
+  created() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData() {
+      this.error = this.event = null;
+      this.loading = true;
+      fetch('http://localhost:3000/events/' + this.id)
+        .then(res => res.json())
+        .then(eventData => {
+          this.event = eventData;
+          this.loading = false;
+          })
+        .catch(e => this.error = e);
+    }
+  }
 }
 </script>
 
