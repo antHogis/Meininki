5e39ca2eedf9534b292a347219a20b6838e73564
Anton Höglund <antonjhoglund@hotmail.com>
2019-10-21 17:40:56 +0300

    Add new view EventSubmitter

---
+++ frontend/src/views/EventSubmitter.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="submit-container">
+    <h1>Add new event</h1>
+    <h2>Title 
+      <span>
+        <input v-model="title">
+      </span>
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  color: white;
+  font-family: 'Open Sans', sans-serif;
+}
+</style>
