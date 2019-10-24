9b4bfc07e4ab23fdb4239b315e23e5cbd3135467
Anton Höglund <antonjhoglund@hotmail.com>
2019-10-24 17:37:13 +0300

    Create post route for creating new user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -8,6 +8,16 @@ function createUser(user) {
     email: user.email,
     password: user.password
   });
-} 
+}
+
+router.post('/register', async (req, res) => {
+  let savedUser;
+  try {
+    savedUser = await createUser(req.body).save();
+    res.send(savedUser);
+  } catch (error) {
+    res.status(500).send({'error': error})
+  }
+});
 
 module.exports = router;
