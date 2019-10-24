a6cdd2255d0f884a7a3c94e4a002dee9250c5f67
Anton Höglund <antonjhoglund@hotmail.com>
2019-10-24 22:08:07 +0300

    Fix error handling of password data type

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -13,41 +13,51 @@ async function createUser(user) {
   });
 }
 
+function createErrorJson(errors) {
+  return {
+    'errors': errors
+  }
+}
+
 router.post('/register', async (req, res) => {
   try {
     let user = await createUser(req.body);
-    let savedUser = await user.save();
-    res.send(savedUser);
-  } catch (error) {
-    let errors = [];
-
-    if (error.errors !== undefined) {
-      for (fieldName in error.errors) {
-        let errorName = error.errors[fieldName].name;
+    try {
+      let savedUser = await user.save();
+      res.send(savedUser);
+    } catch (error) {
+      let errors = [];
   
-        if (errorName === 'CastError') {
-          errors.push(`Invalid data type for ${fieldName}`);
-        }
-  
-        if (errorName === 'ValidatorError') {
-          errors.push(`Invalid ${fieldName} format`);
-        }
-      }
-    }
+      if (error.errors !== undefined) {
+        for (fieldName in error.errors) {
+          let errorName = error.errors[fieldName].name;
     
-    if (error.name === 'MongoError') {
-      if (error.code === 11000) {
-        for (field in error.keyValue) {
-          errors.push(`Duplicate ${field}`);
+          if (errorName === 'CastError') {
+            errors.push(`Invalid data type for ${fieldName}`);
+          }
+    
+          if (errorName === 'ValidatorError') {
+            errors.push(`Invalid ${fieldName} format`);
+          }
         }
       }
+      
+      if (error.name === 'MongoError') {
+        if (error.code === 11000) {
+          for (field in error.keyValue) {
+            errors.push(`Duplicate ${field}`);
+          }
+        }
+      }
+  
+      if (errors.length) {
+        res.status(422).send(createErrorJson(errors));
+      } else {
+        res.status(500).send(createErrorJson(['Unexpected error']));
+      }
     }
-
-    if (errors.length) {
-      res.status(422).send({'error': errors});
-    } else {
-      res.status(500).send({'error': ['Unexpected error']});
-    }
+  } catch (error) {
+    res.status(422).send(createErrorJson(['Invalid data type for password']));
   }
 });
 
