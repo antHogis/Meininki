d19f7efe7f1fa14d5f1f08fa79848f72ae8bdd3f
Anton Höglund <antonjhoglund@hotmail.com>
2019-10-24 19:40:40 +0300

    Create error handling in users/register route
    - Catch CastErrors
    - Catch invalid email format errors
    - Catch unique field duplicate value errors

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -16,7 +16,35 @@ router.post('/register', async (req, res) => {
     savedUser = await createUser(req.body).save();
     res.send(savedUser);
   } catch (error) {
-    res.status(500).send({'error': error})
+    let errors = [];
+
+    if (error.errors !== undefined) {
+      for (fieldName in error.errors) {
+        let errorName = error.errors[fieldName].name;
+  
+        if (errorName === 'CastError') {
+          errors.push(`Invalid data type for ${fieldName}`);
+        }
+  
+        if (fieldName === 'email' && errorName === 'ValidatorError') {
+          errors.push('Invalid email format');
+        }
+      }
+    }
+    
+    if (error.name === 'MongoError') {
+      if (error.code === 11000) {
+        for (field in error.keyValue) {
+          errors.push(`Duplicate ${field}`);
+        }
+      }
+    }
+
+    if (errors.length) {
+      res.status(422).send({'error': errors});
+    } else {
+      res.status(500).send({'error': ['Unexpected error']});
+    }
   }
 });
 
