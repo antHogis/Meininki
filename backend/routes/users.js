745320f1a2100e47a3d66a16f4164e2f8feebbc2
Anton Höglund <antonjhoglund@hotmail.com>
2019-10-24 17:25:46 +0300

    Add function createUser to users.js

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
+
+function createUser(user) {
+  return new User({
+    name: user.name,
+    email: user.email,
+    password: user.password
+  });
+} 
 
 module.exports = router;
