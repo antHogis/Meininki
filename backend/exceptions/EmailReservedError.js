53b2aef49e064cd1e1e71b1177d46b912dbc819c
Anton Höglund <antonjhoglund@hotmail.com>
2019-10-25 15:37:05 +0300

    Add new error class EmailReservedError

---
+++ backend/exceptions/EmailReservedError.js
@@ -0,0 +1,8 @@
+class EmailReservedError extends Error {
+    constructor(address) {
+        super(`Email address ${address} is already in use`);
+        this.name = 'EmailReservedError';
+    }
+}
+
+module.exports = EmailReservedError;
