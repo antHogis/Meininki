ff2f0048e2be5f44b68b81ff3f41b5a706a346ff
Anton Höglund <antonjhoglund@hotmail.com>
2019-10-25 18:34:41 +0300

    Separate value validation criteria to variables
    - Scope: backend, validation.js

--- backend/validation.js
+++ backend/validation.js
@@ -1,22 +1,26 @@
 const Joi = require('@hapi/joi');
 
+const nameValidation = Joi.string()
+  .pattern(/^[a-z0-9_-]{6,16}$/i, {
+    name: 'Must be 6-16 characters, can only contain letters, numbers, underscores and hyphens'
+  });
+
+const emailValidation = Joi.string()
+  .email();
+
+const passwordValidation = Joi.string()
+  .min(8)
+  .max(64);
+
 async function validateRegister(registrationData) {
   const userSchema = Joi.object({
-    name: Joi.string()
-      .pattern(/^[a-z0-9_-]{6,16}$/i, {
-        name: 'Must be 6-16 characters, can only contain letters, numbers, underscores and hyphens'
-      }),
-    email: Joi.string()
-      .email(),
-    password: Joi.string()
-      .min(8)
-      .max(64)
+    name: nameValidation,
+    email: emailValidation,
+    password: passwordValidation
   });
   
   return await userSchema.validateAsync(registrationData, { abortEarly: false });
 }
-
-
 
 module.exports = {
   validateRegister
