b25090b26660f1f32cf5af5717ec520c54960539
Anton Höglund <antonjhoglund@hotmail.com>
2019-10-26 14:27:12 +0300

    Create Form mixin

---
+++ frontend/src/mixins/Form.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  methods: {
+    getErrorByField(field) {
+      if (this.error != null) {
+        let error = this.error.errors.find(e => e.field === field);
+        return error ? error.message : '';
+      } else {
+        return '';
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.submit-container {
+  background-color: #413941;
+  padding: 0% 5%;
+  padding-bottom: 10%;
+}
+
+h1, h2, h3, p, li {
+  color: white;
+  font-family: 'Open Sans', sans-serif;
+}
+</style>
