459faf90cac1882e9e31852a7007a27b1f995cf5
Anton Höglund <antonjhoglund@hotmail.com>
2019-10-26 17:50:15 +0300

    Indent NavBar with 2 spaces

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,77 +1,77 @@
 <template>
-    <div class="TopMenu"> 
-        <div class="Title">
-            <h1> {{ title }} </h1>
-        </div>
-        <div id="nav">
-            <router-link to="/" exact><i class="fas fa-home"></i> Home</router-link>
-            <router-link to="/about" exact>About</router-link>
-            <router-link to="/event/submit">Submit</router-link>
-            <router-link :to="{ name:'logIn' }">Log In</router-link>
-            <router-link :to="{ name:'signUp' }">Sign Up</router-link>
-        </div>
+  <div class="TopMenu"> 
+    <div class="Title">
+      <h1> {{ title }} </h1>
     </div>
+    <div id="nav">
+      <router-link to="/" exact><i class="fas fa-home"></i> Home</router-link>
+      <router-link to="/about" exact>About</router-link>
+      <router-link to="/event/submit">Submit</router-link>
+      <router-link :to="{ name:'logIn' }">Log In</router-link>
+      <router-link :to="{ name:'signUp' }">Sign Up</router-link>
+    </div>
+  </div>
 </template>
 
 <script>
 import cookies from 'js-cookie';
 
 export default {
-    name: 'NavBar',
-    props: ['title']
+  name: 'NavBar',
+  props: ['title']
 }
 </script>
 
 <style scoped>
 .TopMenu {
-    margin: 0px;
-    padding: 0px;
-    width: 100%;
-    background-color: purple;
-    display: inline-block;
-    text-align: center;
+  margin: 0px;
+  padding: 0px;
+  width: 100%;
+  background-color: purple;
+  display: inline-block;
+  text-align: center;
 }
 
 .Title {
-    display: inline-block;
-    text-overflow: clip;
-    overflow: hidden;
+  display: inline-block;
+  text-overflow: clip;
+  overflow: hidden;
 }
 
 .Title > h1 {
-    color: white;
-    font-family: 'Bree Serif', serif;
-    font-size: 4em;
+  color: white;
+  font-family: 'Bree Serif', serif;
+  font-size: 4em;
     
 }
 
 #nav > a {
-    color: white;
-    padding: 0vw 1.25vw 0vw;
-    font-family: 'Rokkitt', serif;
-    text-decoration: none;
-    line-height: 2em;
-    font-size: 1.5em;
-    text-align: right;
-    transition-duration: 0.25s;
-    display: inline-block; /* For transform to work */
+  color: white;
+  padding: 0vw 1.25vw 0vw;
+  font-family: 'Rokkitt', serif;
+  text-decoration: none;
+  line-height: 2em;
+  font-size: 1.5em;
+  text-align: right;
+  transition-duration: 0.25s;
+  display: inline-block; /* For transform to work */
 }
 
 @media (hover: hover) {
-    #nav > a:hover {
-        transform: scale(1.2);
-    }
+  #nav > a:hover {
+    transform: scale(1.2);
+  }
 }
 
 
 .router-link-active {
-    font-weight: bold;
+  font-weight: bold;
 }
 
 @media only screen and (max-width: 260px) {
-    .Title > h1 {
-        font-size: 1em;
-    }
+  .Title > h1 {
+    font-size: 1em;
+  }
 }
 
 </style>
