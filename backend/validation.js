6f14c987e4be0bba0ada7d68d3b511ceb6f0e48d
Anton Höglund <antonjhoglund@hotmail.com>
2019-10-27 11:16:55 +0200

    Add function validateEventQueryParams
    - Scope: backend, validation.js

--- backend/validation.js
+++ backend/validation.js
@@ -42,22 +42,6 @@ async function validateEventQueryParams(queryData) {
   return await querySchema.validateAsync(queryData, { abortEarly: false });
 }
 
-function getJoiValidationErrors(error) {
-  let errorEntries = [];
-
-  if (error.details !== undefined) {
-    for (detail of error.details) {
-      let context = detail.context;
-      let field = context.key;
-      let message = context.name === undefined ? detail.message : context.name;
-      
-      errorEntries.push(new ErrorEntry(field, message));
-    }
-  }
-  
-  return errorEntries;
-}
-
 module.exports = {
   validateRegister,
   validateLogin,
