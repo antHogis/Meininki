295d7537070f0a31fbcf5952ed03acc5795b84df
Anton Höglund <antonjhoglund@hotmail.com>
2019-10-27 11:03:02 +0200

    Specify required fields in validation.js

--- backend/validation.js
+++ backend/validation.js
@@ -3,14 +3,17 @@ const Joi = require('@hapi/joi');
 const nameValidation = Joi.string()
   .pattern(/^[a-z0-9_-]{6,16}$/i, {
     name: 'Must be 6-16 characters, can only contain letters, numbers, underscores and hyphens'
-  });
+  })
+  .required();
 
 const emailValidation = Joi.string()
-  .email();
+  .email()
+  .required();
 
 const passwordValidation = Joi.string()
   .min(8)
-  .max(64);
+  .max(64)
+  .required();
 
 async function validateRegister(registrationData) {
   const userSchema = Joi.object({
