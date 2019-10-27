b84aa4c8257b85b20448cfbeb924281e6cadfb8d
Anton Höglund <antonjhoglund@hotmail.com>
2019-10-27 21:00:29 +0200

    Update route PUT /events/id
    - Scope: backend, events.js
    - Add validation and authorization to route

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
 const Event = require('../models/Event');
 const { verifyToken } = require('../authorization');
-const { validateEventQueryParams, getJoiValidationErrors } = require('../validation');
+const {
+  validateEventQueryParams,
+  validateEvent,
+  getJoiValidationErrors,
+} = require('../validation');
 const { ErrorResponse } = require('../errors/ErrorResponse');
+const { UnauthorizedError } = require('../errors/errors');
 
 function createEvent(event, owner) {
   return new Event({
@@ -47,10 +52,34 @@ router.post('/', verifyToken, (req, res) => {
     .catch(err => res.status(422).send({ error: err}));
 });
 
-router.put('/:id', (req, res) => {
-  Event.findByIdAndUpdate(req.params.id, req.body)
-    .then(data => res.status(200).send())
-    .catch(err => res.status(500).send({error: err}));
+router.put('/:id', verifyToken, async (req, res) => {
+  try {
+    await validateEvent(req.body);
+    let event = await Event.findById(req.params.id);
+
+    if (event.owner !== req.user._id) {
+      console.log(`Owner ${event.owner}, User ${req.user}`);
+      throw new UnauthorizedError();
+    } 
+
+    await Event.findByIdAndUpdate(req.params.id, req.body)
+    res.status(200).send();
+  } catch (error) {
+    console.log(error);
+    let errorResponse = new ErrorResponse();
+
+    if (error instanceof UnauthorizedError) {
+      errorResponse.add('token', error.message);
+    }
+
+    errorResponse.addAll(getJoiValidationErrors(error));
+
+    if (errorResponse.hasEntries()) {
+      res.status(422).send(errorResponse.compile());
+    } else {
+      res.status(500).send(ErrorResponse.default());
+    }
+  }
 });
 
 module.exports = router;
