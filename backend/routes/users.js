cc7da0a2a38758d9819a149d5593ebacd3460d11
Anton Höglund <antonjhoglund@hotmail.com>
2019-10-29 01:10:23 +0200

    Implement route PUT /users/update

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,7 +3,11 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const User = require('../models/User');
-const { validateRegister, validateLogin, getJoiValidationErrors } = require('../validation');
+const {
+  validateRegister, 
+  validateLogin,
+  validateUserUpdate,
+  getJoiValidationErrors } = require('../validation');
 const { verifyToken } = require('../authorization');
 const { ErrorResponse, ErrorEntry } = require('../errors/ErrorResponse');
 const { 
@@ -117,6 +121,64 @@ router.get('/:id', async (req, res) => {
     res.send(createUserBody(user));
   } else {
     res.status(404).send(new ErrorResponse().add('id', 'Invalid id').compile());
+  }
+});
+
+router.put('/update', verifyToken, async (req, res) => {
+  if (!req.user) {
+    return res.status(500).send(ErrorResponse.default());
+  }
+  try {
+    let user = await User.findById({_id: req.user._id});
+    
+    if (!user) {
+      throw new InvalidTokenIdError();
+    }
+
+    await validateUserUpdate(req.body, user);
+    
+    if (req.body.password 
+      && !(await bcrypt.compare(req.body.oldPassword, user.password))) {
+        throw new PasswordIncorrectError();
+    } else if (req.body.password) {
+      req.body.password = await bcrypt.hash(req.body.password, 10);
+    }
+
+    if (req.body.email
+      && await User.findOne({ email: req.body.email })) {
+      throw new EmailReservedError(req.body.email);
+    }
+
+    let update = {};
+    if (req.body.password) update['password'] = req.body.password;
+    if (req.body.name) update['name'] = req.body.name;
+    if (req.body.email) update['email'] = req.body.email;
+
+    await User.findByIdAndUpdate(req.user._id, update);
+    res.send('OK');
+  } catch (error) {
+    let errorResponse = new ErrorResponse();
+
+    if (error instanceof InvalidTokenIdError) {
+      errorRespsonse.add('token', error.message);
+    }
+
+    if (error instanceof PasswordIncorrectError) {
+      errorResponse.add('password', error.message);
+    }
+
+    if (error instanceof EmailReservedError) {
+      errorResponse.add('email', error.message);
+    }
+    
+    errorResponse.addAll(getJoiValidationErrors(error));
+
+    if (errorResponse.hasEntries()) {
+      res.status(422).send(errorResponse.compile());
+    } else {
+      console.log(error);
+      res.status(500).send(ErrorResponse.default());
+    }
   }
 });
 
